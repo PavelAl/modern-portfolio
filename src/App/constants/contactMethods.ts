8a8fa03780930c29bd7b81a8aa25a00c0d3e7b9d
PavelAl <PavelAl@users.noreply.github.com>
2025-06-30 13:39:39 -0400

    Version update (#10)

--- src/App/constants/contactMethods.ts
+++ src/App/constants/contactMethods.ts
@@ -4,19 +4,19 @@ export const contactMethods: ContactMethod[] = [
   {
     icon: ["fab", "linkedin"],
     color: "#0077B5",
-    link: "https://linkedin.com",
+    link: "https://www.linkedin.com/in/paveloparin",
     label: "LinkedIn",
   },
   {
     icon: ["fab", "github"],
     color: "#333",
-    link: "https://github.com",
+    link: "https://github.com/PavelAl",
     label: "GitHub",
   },
   {
     icon: "envelope",
     color: "#EA4335",
-    link: "mailto:contact@example.com",
+    link: "mailto:oparinpavelal@gmail.com",
     label: "Gmail",
   },
 ];
